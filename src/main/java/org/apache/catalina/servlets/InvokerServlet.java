b200042c58d29706a1a23ede089535fe792a9cf0
Jordi Murgo <jordi.murgo@gft.com>
2016-08-26 12:09:14 +0200

    Dummy InvokerServlet

---
+++ src/main/java/org/apache/catalina/servlets/InvokerServlet.java
@@ -0,0 +1,10 @@
+package org.apache.catalina.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by jpmo on 26/08/2016.
+ */
+public class InvokerServlet extends HttpServlet {
+
+}
